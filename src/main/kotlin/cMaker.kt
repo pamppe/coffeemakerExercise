002ea647240caa40ae3605d0edfd1fbd8dbdf1e5
Emil <emil.lehikoinen@outlook.com>
2023-01-24 15:54:29 +0200

    Initial commit

---
+++ src/main/kotlin/cMaker.kt
@@ -0,0 +1,33 @@
+class CoffeeMaker(val coffeeCapacity: Int = 50, val waterCapacity: Int = 150) {
+    var coffeeAmount: Int = 0
+    var waterAmount: Int = 0
+    var isOn: Boolean = false
+        private set
+
+    fun makeDoubleEspresso() {
+        if (isOn) {
+            if (coffeeAmount >= 16 && waterAmount >= 30) {
+                coffeeAmount -= 16
+                waterAmount -= 30
+                println("Double espresso is ready!")
+            } else {
+                println("Not enough ingredients. Please add more coffee or water.")
+            }
+        } else {
+            println("Coffee maker is not on.")
+        }
+    }
+    fun makeRegular() {
+        if (isOn) {
+            if (coffeeAmount >= 10 && waterAmount >= 120) {
+                coffeeAmount -= 10
+                waterAmount -= 120
+                println("Regular coffee is ready!")
+            } else {
+                println("Not enough ingredients. Please add more coffee or water.")
+            }
+        } else {
+            println("Coffee maker is not on.")
+        }
+    }
+}
